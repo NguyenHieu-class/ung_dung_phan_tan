ed07731afd6b309e83a28ae8a5e22990a39a0efd
Your Name <22010103@st.phenikaa-uni.edu.vn>
2024-12-30 14:46:16 +0700

    chord

--- DS_Lec_4_Dinh_Danh/chord.py
+++ DS_Lec_4_Dinh_Danh/chord.py
@@ -1,118 +1,116 @@
-#Chord DHT implementation in Python
+# Chuong trinh mo phong thuat toan Chord DHT
 from pprint import pprint
 
-def binarySearch(alist, item):                                                                                                    # Tìm kiếm nhị phân trong mảng đã sắp xếp
+def binarySearch(arr, item):                                                                # Ham tim kiem nhi phan
     first = 0
-    last = len(alist)-1
+    last = len(arr) - 1
     found = False
-    
-    while first<=last and not found:
-        midpoint = (first + last)//2
-        if alist[midpoint] == item:
+    while (first <= last and not found):
+        mid = (first + last) // 2
+        if arr[mid] == item:
             found = True
         else:
-            if item < alist[midpoint]:
-                last = midpoint-1
+            if item < arr[mid]:
+                last = mid - 1
             else:
-                first = midpoint+1
-    
-    return found, midpoint
+                first = mid + 1
+    return found, mid
 
-def successor(L, n):                                                                                                                # Tìm node kế tiếp của n trong ring, nếu n không tồn tại thì trả về None
-    found, midpoint = binarySearch(L,n)
-    if L[midpoint] >= n:
-        return L[midpoint]
-    if L[midpoint] < n:
-        if midpoint==len(L)-1:
+def successor(L, n):                                                                        # Ham tim kiem successor
+    found, mid = binarySearch(L, n)
+    if L[mid] >= n:
+        return L[mid]
+    if L[mid] < n:
+        if mid == len(L) - 1:
             return None
-        return L[midpoint+1]
+        return L[mid + 1]
 
-
-def get_fingers(nodes, m):                                                                                                        # Tạo bảng finger cho mỗi node trong ring
+def get_fingers(nodes, m):                                                                  # Tao bang finger
     nodes_augment = nodes.copy()
     nodes_augment = sorted(nodes_augment)
-    maxnum = pow(2,m)
-    nodes_augment.append(nodes_augment[0]+maxnum)
-    
+    max_node = pow(2, m)
+
+    # wraparound first node to accommodate nodes clockwise of n when n is the larger than the largest value node
+    nodes_augment.append(nodes_augment[0] + max_node)
+
     finger_table = {}
-    for node in nodes: 
-        fingers = [None]*m
+    for node in nodes:
+        fingers = [None] * m
         for i in range(m):
-            j = (node + pow(2,i))%maxnum
-            fingers[i] = successor(nodes_augment,j)%maxnum
+            j = (node + pow(2, i)) % max_node
+            fingers[i] = successor(nodes_augment, j)%max_node
         finger_table[node] = fingers.copy()
-        
     return finger_table
 
-def get_key_loc(nodes, k, m):                                                                                                    # Tìm vị trí của key k trong ring
+def get_key_loc(nodes, k, m):                                                            # Tim vi tri cua key
     nodes_augment = nodes.copy()
     nodes_augment = sorted(nodes_augment)
-    maxnum = pow(2,m)
-    nodes_augment.append(nodes_augment[0]+maxnum)
-    return successor(nodes_augment, k)%maxnum
+    max_node = pow(2, m)
 
+    # wraparound first node to accommodate nodes clockwise of n when n is the larger than the largest value node
+    nodes_augment.append(nodes_augment[0] + max_node)
+    return successor(nodes_augment, k)%max_node
 
-def get_query_path(nodes, k, n, m):                                                                                             # Tìm đường đi từ node n đến vị trí của key k
+def get_query_path(nodes, k, n, m):                                                      # Tim duong truy van
     nodes_copy = nodes.copy()
     nodes_copy = sorted(nodes_copy)
-    
-    k_loc = get_key_loc(nodes_copy, k, m)
-    fingers = get_fingers(nodes_copy, m)
-    query_path = [n]
-    
-    while(True):
-        
-        if k_loc==n:
-            return query_path
 
+    # vi tri cua key
+    key_loc = get_key_loc(nodes_copy, k, m)
+
+    # finger table
+    fingers = get_fingers(nodes_copy, m)
+
+    # bat dau tim duong truy van tu node co ban
+    query_path = [n]
+
+    while (True):
+        
+        # return duong truy van neu tim thay key
+        if key_loc == n:
+            return query_path
+        
         f = fingers[n]
-        
-        i = 0
+
+        i=0
         x = f[i]
-        
-        if k_loc > n: 
-            while (x < k_loc) and (x > n):
+
+        if key_loc > n:
+            
+            # Neu duong dan clockwise nam giua node co ban vaf vi tri cua key ma khong phai 0, tat ca cac node nam giua se la duong dan truy van
+            # duong dan (x) phai nam trong khoang key_loc > x > x
+            while (x <= key_loc) and (x > n):
                 i += 1
                 if i == len(f):
                     break
                 x = f[i]
-                
-        if k_loc < n: 
-            while ( ((x > k_loc) and (x > n)) or ((x < k_loc) and (x < n)) ):
+
+        if key_loc < n:
+            # Neu duong dan counter-clockwise nam giua node co ban vaf vi tri cua key ma khong phai 0, tat ca cac node nam giua se la duong dan truy van
+            # duong dan (x) phai nam trong khoang (key_loc < x) && (n < x) OR (key_loc > x) && (n > x)
+            while (x >= key_loc) and (x < n):
                 i += 1
                 if i == len(f):
                     break
                 x = f[i]
-        if i==0:
+
+        # Them node tiep theo vao duong dan truy van
+        if i == 0:
             n = f[0]
         else:
             n = f[i-1]
-            
         query_path.append(n)
-    
-if __name__=="__main__":                                                                                                            # Test
-    
-    nodes = [112, 96, 80, 16, 32, 45] # nodes in the P2P system
-    m = 7 # Ring is of size 2^m
-    print('Chord DHT with peers {nodes}, and m = {m} \n'.format(nodes=nodes, m=7))
-    
-    print('The finger table for this DHT is: ')
-    pprint(get_fingers(nodes, m))
-    print()
-    
-    file_key = 42
-    print('File location for file key {file_key} is at node {file_key_location} \n'.format(
-        file_key=file_key, file_key_location=get_key_loc(nodes, file_key, m)
-    ))
-    query_node = 80
-    print('Query path for file {file_key} with origin node {query_node} : {query_path}'.format(
-        file_key=file_key, query_node=query_node, query_path=get_query_path(nodes, file_key, query_node, m)
-    ))
-    query_node = 96
-    print('Query path for file {file_key} with origin node {query_node} : {query_path}'.format(
-        file_key=file_key, query_node=query_node, query_path=get_query_path(nodes, file_key, query_node, m)
-    ))
-    query_node = 45
-    print('Query path for file {file_key} with origin node {query_node} : {query_path}'.format(
-        file_key=file_key, query_node=query_node, query_path=get_query_path(nodes, file_key, query_node, m)
-    ))
+
+if __name__ == "__main__":
+    # Test case
+
+    nodes = [143, 90, 80, 15, 31, 46]                                                      # list node
+    m = 7
+    print("Chord DHT: ", nodes, "m = ", m, "\n")
+    print("Finger Table:\n")
+    pprint(get_fingers(nodes, m))                                                          # in bang finger
+    print("\n")
+
+    key = 44                                                                               # key can tim
+    print("Key location for key = ", key, "is at node: ", get_key_loc(nodes, key, m), "\n")
+
